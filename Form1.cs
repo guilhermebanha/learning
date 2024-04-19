7defd1560662068b296d42b534737d66b22ed348
Guilherme Banha <guilherme.epbjc23@gmail.com>
2024-04-19 13:13:20 +0100

    Push Intial FIles

---
+++ Form1.cs
@@ -0,0 +1,22 @@
+namespace learning;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    ~Form1()
+    {
+        MessageBox.Show("Form1 disposed");
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        this.Hide();
+        Form2 f2 = new Form2(textBox1.Text,textBox2.Text);
+        f2.ShowDialog();
+        this.Close();
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace learning
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string a, string b)
+        {
+            InitializeComponent();
+            Label l1 = new Label();
+            l1.Location = new Point(100, 100);
+            l1.Text = a;
+            Label l2 = new Label();
+            l2.Location = new Point(100, 200);
+            l2.Text = b;
+            Controls.Add(l1);
+            Controls.Add(l2);
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
+            this.Close();
+        }
+    }
+}
